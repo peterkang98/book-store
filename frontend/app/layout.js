6d77db291078dc9d75f58a999f40a2e2296aaae0
강동현 <101819709+peterkang98@users.noreply.github.com>
2023-08-04 15:41:07 +0900

    Setup shadcn

--- frontend/app/layout.js
+++ frontend/app/layout.js
@@ -4,13 +4,12 @@ import { Inter } from 'next/font/google'
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
-  title: 'Online Book Store',
-  description: '',
+  title: '온라인 서점'
 }
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
+    <html lang="ko">
       <body className={inter.className}>{children}</body>
     </html>
   )
